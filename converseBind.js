8979650bf3fd45d219c3c0d50828978d4428b7e6
Simon Pettersen Nguyen <Simon@Simons-MacBook-Air.local>
2015-03-20 01:17:10 +0100

    login script and bind script added

---
+++ converseBind.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    if(getCookie("sid")!=""){
+        $('#header').hover(function(evt) {
+            $('a[href="../dhis-web-commons-security/logout.action"]').attr("onclick", "logOut()");
+            $('a[href="../dhis-web-commons-security/logout.action"]').attr("href", null);
+        });
+        require(['converse'], function(converse) {
+            converse.initialize({
+                prebind: true,
+                keepalive: true,
+                bosh_service_url: 'https://hmis.moh.gov.rw/http-bind/',
+                jid: getCookie("jid"),
+                sid: getCookie("sid"),
+                rid: getCookie("rid"),
+                allow_otr: false,
+                allow_muc: false,
+                animate: false
+            });
+        });
+    }
+});
+
+function logOut() {
+	try {
+		converse.logOut();
+	} catch (err) {}
+	deleteCookie("sid");
+	deleteCookie("jid");
+	deleteCookie("rid");
+	window.location = "../dhis-web-commons-security/logout.action"
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1);
+		if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+	}
+	return "";
+}
+
+function deleteCookie(cname) {
+    document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
---
+++ converselogin.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+        $('#loginForm').on('submit', function(event) {
+            event.preventDefault();
+            logInToDHIS2();
+        });
+});
+
+function logInToDHIS2() {
+	$.ajax({
+		url: "../../dhis-web-commons-security/login.action",
+		type: "POST",
+		dataType: 'text',
+		context: $('#loginForm'),
+		data: $('#loginForm').serialize(),
+		success: function(data, textStatus, jqXHR) {
+			checkIfUserIsDistrictHospitalHMISdatamanager();
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+		}
+	});
+}
+
+function checkIfUserIsDistrictHospitalHMISdatamanager() {
+	var isDistrictHospitalHMISdatamanager = false;
+	$.ajax({
+		dataType: "json",
+		type: "GET",
+		url: "../../api/me",
+		success: function(data, textStatus, jqXHR) {
+            if (data.userCredentials.userAuthorityGroups[0].name == "District Hospital HMIS data managers") {
+                isDistrictHospitalHMISdatamanager = true;
+            } 
+            else if(data.id=="HkhYnzFnuSs"){
+                isDistrictHospitalHMISdatamanager = true;
+            }
+	    else {
+                isDistrictHospitalHMISdatamanager = false;
+            }
+			if (isDistrictHospitalHMISdatamanager) {
+				logInToOpenfireServer();
+			} else {
+                window.location.href = "../../dhis-web-dashboard-integration/index.action";
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+            window.location.href = "../../dhis-web-commons-security/login.action?failed=true";
+		}
+	});
+}
+
+function logInToOpenfireServer() {
+	$.ajax({
+		url: "https://hmis.moh.gov.rw/prebind/LoginServlet",
+		type: "POST",
+		dataType: 'text',
+		context: $('#loginForm'),
+		data: $('#loginForm').serialize(),
+		success: function(data, textStatus, jqXHR) {
+			window.location.href = "../../dhis-web-dashboard-integration/index.action";
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			window.location.href = "../../dhis-web-dashboard-integration/index.action";
+		}
+	});
+}
